4fee6704c109b2e4598c759f603a57966c7bcf13
Mürsel Elibol <murselelibol@gmail.com>
2024-10-10 23:40:26 +0300

    feat: init vue app with composition api

---
+++ vue/.eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+    root: true,
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        'vue/multi-word-component-names': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        indent: ['error', 4],
+        semi: ['error', 'always']
+    }
+};
---
+++ vue/src/pages/Home.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="h-screen w-screen flex justify-center items-center">
+        <span class="text-5xl">Hi 👋</span>
+    </div>
+</template>
