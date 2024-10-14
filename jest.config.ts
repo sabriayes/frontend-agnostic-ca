42492290738c99e082a8dd16b0e61884dc53507f
Sabri Ayeş <sabri@naylalabs.com>
2024-10-14 22:36:07 +0300

    refactor: reorganize monorepo structure

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,19 @@
 import { Config } from 'jest';
 
 const config: Config = {
+    rootDir: __dirname,
     preset: 'ts-jest',
     testEnvironment: 'node',
     verbose: true,
     moduleFileExtensions: ['ts', 'js'],
     testMatch: ['**/tests/**/*.spec.ts'],
     testPathIgnorePatterns: ['node_modules'],
+    projects: ["<rootDir>/libs/*/jest*config.js"],
     transform: {
         '^.+\\.ts?$': 'ts-jest',
     },
     moduleNameMapper: {
-        '^@libs/common/(.*)$': '<rootDir>/$1',
+        '^@libs/auth/(.*)$': '<rootDir>/src/$1',
     },
 };
 
--- libs/auth/src/inversify.config.ts
+++ libs/auth/src/inversify.config.ts
@@ -3,7 +3,7 @@ import { IAuthRepository, ILoginUseCase } from '@libs/auth/application/ports';
 import { AuthRepository } from '@libs/auth/infra/repositories';
 import { LoginUseCase } from '@libs/auth/application/usecases';
 
-const container = new Container({ defaultScope: 'Singleton' })
+const container = new Container({ defaultScope: 'Singleton' });
 
 container.bind<IAuthRepository>('IAuthRepository').to(AuthRepository);
 container.bind<ILoginUseCase>('ILoginUseCase').to(LoginUseCase);
