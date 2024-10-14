b3f3ca90b8c2b162544bfbec7f909bb9061d54e8
Sabri Ayeş <sabri@naylalabs.com>
2024-10-14 23:14:33 +0300

    refactor: reorganize test structure

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,24 @@
 import { Config } from 'jest';
 
 const config: Config = {
-    rootDir: __dirname,
     preset: 'ts-jest',
-    testEnvironment: 'node',
     verbose: true,
+    testEnvironment: 'node',
     moduleFileExtensions: ['ts', 'js'],
-    testMatch: ['**/tests/**/*.spec.ts'],
-    testPathIgnorePatterns: ['node_modules'],
-    projects: ["<rootDir>/libs/*/jest*config.js"],
     transform: {
         '^.+\\.ts?$': 'ts-jest',
     },
-    moduleNameMapper: {
-        '^@libs/auth/(.*)$': '<rootDir>/src/$1',
-    },
+    projects: [
+        {
+            preset: 'ts-jest',
+            displayName: 'common',
+            testMatch: ['<rootDir>/libs/common/**/__tests__/**/*.spec.ts'],
+            testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+            moduleNameMapper: {
+                '^@libs/common/(.*)$': '<rootDir>/libs/common/src/$1',
+            },
+        },
+    ],
 };
 
 export default config;
