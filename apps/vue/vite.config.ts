5b65e8aa0ab51b809c5c8f516d670112c9506b8b
Sabri Ayeş <sabri@naylalabs.com>
2024-10-26 00:02:06 +0300

    fix(vue): reformat config files

--- apps/vue/vite.config.ts
+++ apps/vue/vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
