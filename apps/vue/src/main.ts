a855e2ef417aa1c0a21498b1499559c037607285
Sabri Ayeş <sabri@naylalabs.com>
2024-10-26 01:12:23 +0300

    fix(vue): add @ alias to import paths

--- apps/vue/src/main.ts
+++ apps/vue/src/main.ts
@@ -2,8 +2,8 @@ import 'reflect-metadata';
 import '@/assets/main.scss';
 
 import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import App from '@/App.vue';
+import router from '@/router';
 
 const app = createApp(App);
 app.use(router);
