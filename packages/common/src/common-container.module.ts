c57970678171e577d58b84b86239cf02da128f70
Sabri Ayeş <sabri@naylalabs.com>
2024-12-02 14:55:59 +0300

    feat(common): create concrete store service

--- packages/common/src/common-container.module.ts
+++ packages/common/src/common-container.module.ts
@@ -4,9 +4,24 @@ import {
     ConfigService,
     ICommonConfigService,
 } from '@core/common/config';
+import {
+    STORE_SERVICE_TOKEN,
+    TOKENS_SERVICE_TOKEN,
+} from '@core/common/shared/const';
+import { StoreService, TokensService } from '@core/common/shared/services';
+import { ICommonStoreService } from '@core/common/base';
+import { ITokensService } from '@core/common/shared/ports';
 
 export const commonContainerModule = new ContainerModule((bind: interfaces.Bind) => {
     bind<ICommonConfigService>(CONFIG_SERVICE_TOKEN)
         .to(ConfigService)
+        .inSingletonScope();
+
+    bind<ICommonStoreService>(STORE_SERVICE_TOKEN)
+        .to(StoreService)
+        .inSingletonScope();
+
+    bind<ITokensService>(TOKENS_SERVICE_TOKEN)
+        .to(TokensService)
         .inSingletonScope();
 });
